fb529d4a448fbdea084ac515b80ef56d844976bf
nahuel <pnahuel568@gmail.com>
2026-06-09 21:54:18 -0300

    primer comit

---
+++ app.py
@@ -0,0 +1,19 @@
+import random
+
+print("Adivina el número (1-100)")
+secreto = random.randint(1, 100)
+intentos = 0
+
+while True:
+    try:
+        n = int(input("Tu número: "))
+        intentos += 1
+        if n < secreto:
+            print("Más alto")
+        elif n > secreto:
+            print("Más bajo")
+        else:
+            print(f"Ganaste en {intentos} intento(s)")
+            break
+    except ValueError:
+        print("Número inválido")
